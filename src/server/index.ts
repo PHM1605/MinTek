ce0fa46a9e74f815bdeb453a65dd892ebea0b5ab
unknown <PHM1605@gmail.com>
2024-02-02 00:08:37 +0700

    small updates

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,15 +7,15 @@ const app = express();
 const PORT = 3000;
 app.use(api);
 
-// For local
-app.listen(PORT, ()=>console.log("Started"));
+// // For local
+// app.listen(PORT, ()=>console.log("Started"));
 
-// // For public
-// app.use(express.static(process.cwd() + "/dist"));
-// const sslServer = https.createServer({
-//     key: fs.readFileSync('./cert/key.pem'),
-//     cert: fs.readFileSync('./cert/cert.pem')
-// }, app);
-// sslServer.listen(PORT, ()=>
-//     console.log(`Example app listening on https://mintek.top`)
-// );
+// For public
+app.use(express.static(process.cwd() + "/dist"));
+const sslServer = https.createServer({
+    key: fs.readFileSync('./cert/key.pem'),
+    cert: fs.readFileSync('./cert/cert.pem')
+}, app);
+sslServer.listen(PORT, ()=>
+    console.log(`Example app listening on https://mintek.top`)
+);
