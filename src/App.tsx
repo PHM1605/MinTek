140b40574247f4975087c5fed847af72a716981b
RTDevOps <RTDevOps@dmspro.vn>
2024-02-02 17:42:00 +0700

    some bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import {useState, useEffect, FormEvent} from "react";
+import {Container, Navbar} from 'react-bootstrap';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-
   return (
-    <div>
-      <h1>Todos</h1>
+    <div className="App">
+      <Navbar expand="lg" bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img alt="" src="/src/assets/logo.jpg" width="80" height="40" className="d-inline-block align-top"/>
+            MinTek
+          </Navbar.Brand>
+          
+        </Container>
+      </Navbar>
     </div>
-  )
+  );
 }
 
 export default App
